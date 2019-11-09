9f199134f879e8507fa844e1ec68a957d4bd9222
Jungeun <kjeunn@gmail.com>
2019-11-10 00:24:40 +0900

    Implement list CRUD :sparkles:

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,34 @@ const db = require("../db");
 const user = require("./user");
 
 // add list
-router.post("/list", function(req, res) {});
+router.post("/list", async (req, res) => {
+  const createdList = await db.list.post(req.body);
+  if (createdList === "failure") {
+    res.json("failure");
+  } else {
+    res.json("success");
+  }
+});
 
 // update list
-router.put("/list", function(req, res) {});
+router.put("/list", async (req, res) => {
+  const updatedList = await db.list.put(req.body);
+  if (updatedList === "failure") {
+    res.json("failure");
+  } else {
+    res.json("success");
+  }
+});
 
 // delete list
-router.delete("/list", function(req, res) {});
+router.delete("/list", async (req, res) => {
+  const deletedList = await db.list.delete(req.body);
+  if (deletedList === "failure") {
+    res.json("failure");
+  } else {
+    res.json("success");
+  }
+});
 
 // add card
 router.post("/card", function(req, res) {});
